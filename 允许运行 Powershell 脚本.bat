204777a471feec43c1c08b4ad6a0ae64973e390a
sakurakugu <171350650@qq.com>
2025-04-12 22:48:43 +0800

    更改名称，添加编码检测

--- 允许运行 Powershell 脚本.bat
+++ 允许运行 Powershell 脚本.bat
@@ -1,7 +1,14 @@
 @REM 该脚本用于允许运行ps1脚本
 
 @echo off
-chcp 65001 >nul
+@REM 检测并设置字符编码
+for /f "tokens=2 delims=:" %%a in ('chcp') do set cp=%%a
+set cp=%cp: =%
+if "%cp%"=="936" (
+    chcp 936 >nul
+) else (
+    chcp 65001 >nul
+)
 
 @REM 输出原先的执行策略，设置当前的执行策略，查看当前的执行策略
 powershell -Command ^
